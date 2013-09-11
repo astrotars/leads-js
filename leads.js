07c9383f6612dedc01502f681ddf0f0db3590267
Dustin Higginbotham <dustinh052@gmail.com>
2013-09-11 09:13:43 -0600

    A big rewrite of internals.  Grouping everything inside of a Leads function

--- leads.js
+++ leads.js
@@ -1,272 +1,233 @@
-(function() {
+var Leads = function(cookieName, formPrefix, debug) {
 
     /**
      * Sets cookie prefix if LEADS_COOKIE_NAME global is set. Defaults to __gfLeads.
      */
+    this.cookieName = cookieName;
+    this.prefix = formPrefix;
+    this.debug = debug;
 
-    var cookieName = (window.LEADS_COOKIE_NAME || '__gfLeads'),
-        prefix = (window.LEADS_PREFIX || 'gf_'),
-        debug = (window.LEADS_DEBUG || false);
+    this.searchEngines = ['google', 'bing', 'ask', 'yahoo'];
 
-    /**
-     * Referring search engines
-     */
-    var search_engines = [
-        'google', 'bing', 'ask', 'yahoo'
+    this.info = JSON.parse(this.getCookie()); // we will store pretty much every piece of info in here
+    this.output = {};
+
+    this.inputNames = [ // the fields we will be looking for
+        'external_url', 
+        'internal_url', 
+        'medium', 
+        'source', 
+        'campaign', 
+        'term', 
+        'content'
     ];
 
+    for (var i = 0; i <= this.inputNames.length; i++) {
+        this.output[this.inputNames[i]] = (this.info[this.inputNames[i]] || '');
+    }
 
-    /**
-     * Debugging purposes
-     */
+    if (!this.isSameHost()) {
+        this.process();
+    } else {
+        this.output.internal_url = this.getInternalURL() || '';
+        this.save();
+    }
+};
 
-    var log = function() {
+Leads.prototype.process = function() {
+    this.output.external_url = this.getExternalURL() || '';
+    this.output.internal_url = this.getInternalURL() || '';
 
-        if (debug) {
+    var medium = this.getMedium(),
+        query  = window.location.search;
 
-            var args = [prefix + 'leads debug:'];
+    this.output.medium = 'other';
 
-            for (var i = 0; i < arguments.length;  i++) {
-                args.push(arguments[i]);
-            }
+    if (medium.type == 'seo') {
+        this.output.medium = 'seo';
+        this.output.term = this.getSEOData() || '';
+        this.output.source = medium.source;
+    }
 
-            console.log(args);
+    if (medium.type == 'ppc') {
+        this.output.medium = 'ppc';
+        this.output.term = this.getParameterByName('utm_term');
+    }
 
+    var extra = this.getExtraData();
+    this.output.campaign = extra.campaign;
+    this.output.content = extra.content;
+
+    this.save();
+};
+
+Leads.prototype.getElement = function(selector) {
+    return document.querySelectorAll(selector);
+};
+
+Leads.prototype.getExternalURL = function() {
+    return !this.isSameHost() ? document.referrer : false;
+};
+
+Leads.prototype.getInternalURL = function() {
+    return this.isSameHost() ? document.referrer : false;
+};
+
+Leads.prototype.log = function() {
+    if (this.debug) {
+        var args = [this.prefix + 'leads debug:'];
+        for (var i = 0; i < arguments.length; i++) {
+            args.push(arguments[i]);
         }
+        console.log(args.join(' '));
+    }
+};
 
-    };
+Leads.prototype.writeCookie = function(value, expires) {
+    var date = new Date();
+    date.setDate(date.getDate() + expires); // set an expiration date
 
-    if (!document.querySelectorAll) {
-        log('leads.js cannot run without browser support for querySelectorAll');
+    var data = escape(value) + ((expires === null) ? '' : '; expires='+ date.toUTCString());
+    document.cookie = this.cookieName + "=" + data; // write the cookie
+};
+
+Leads.prototype.getCookie = function() {
+    var cookies = document.cookie;
+    // Our regex matches the name of the cookie and then pulls out the content all the way until the semicolon
+    var matches = cookies.match(new RegExp('('+ this.cookieName + ')\=([^\;]+)', 'i'));
+
+    // no cookie found
+    if (!matches || matches.length === 0) {
         return false;
     }
 
+    matches.shift(); // remove the first item from the results array (which is the pattern)
 
-    /**
-     * Writes cookie -- We need data to persist across different page loads.
-     */
+    return unescape(matches[1]); // cookies are escaped, so fix that
+};
 
-    var writeCookie = function(name, value, expires) {
+Leads.prototype.parseURI = function(uri) {
+    // Apparently, if you simply create an anchor element, the browser creates a parsed URI object based on it
+    var a = document.createElement('a'); 
+    a.href = uri;
 
-        var date = new Date();
-        date.setDate(date.getDate() + expires); // set an expiration date
+    return a;
+};
 
-        var data = escape(value) + ((expires === null) ? '' : '; expires='+ date.toUTCString());
-        document.cookie = name + "=" + data; // write the cookie
+Leads.prototype.isSameHost = function() {
 
-    };
+    return (window.location.host == this.parseURI(document.referrer).host);
+};
 
+Leads.prototype.getMedium = function() {
 
-    /**
-     * Gets and returns a specific cookie from the browser
-     */
-
-    var getCookie = function(name) {
-
-        var cookies = document.cookie;
-        // Our regex matches the name of the cookie and then pulls out the content all the way until the semicolon
-        var matches = cookies.match(new RegExp('('+ name + ')\=([^\;]+)', 'i'));
-
-        // no cookie found
-        if (!matches || matches.length === 0) {
-            return false;
-        }
-
-        matches.shift(); // remove the first item from the results array (which is the pattern)
-
-        return unescape(matches[1]); // cookies are escaped, so fix that
-
-    };
-
-    /**
-     * Force Javascript to parse out the URI passed over to it by creating
-     * an anchor element.
-     */
-
-    var parseURI = function(uri) {
-
-        // Apparently, if you simply create an anchor element, the browser creates a parsed URI object based on it
-        var a = document.createElement('a'); 
-        a.href = uri;
-
-        return a;
-
-    };
-
-
-    /**
-     * Return a GET/Query parameter by name
-     */
-
-    var getParameterByName = function(name, queryString) {
-
-        // Thanks to Stackoverflow for the bulk of this code.  Looks like we are adjusting the potential arrays
-        // inside of our query string
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-
-        // Based on the property name grab the potential content immediately following it.
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-            results = regex.exec((queryString || window.location.search));// handle if they pass a URI directly or just use the current one
-
-        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); // decode what we have returned
-
-    };
-
-    var $ = function(selector) {
-        return document.querySelectorAll(selector);
-    };
-
-
-    /**
-     * Start setting up our info object to pass to the form
-     */
-
-    var info = {}, // we will store pretty much every piece of info in here
-        inputNames = [ // the fields we will be looking for
-            'external_url', 
-            'internal_url', 
-            'medium', 
-            'source', 
-            'campaign', 
-            'term', 
-            'content'
-        ];
-
-    // pre-populate fields to blank strings
-    for (var i = 0; i < inputNames.length; i++) {
-        info[inputNames[i]] = '';
-    }
-
-
-    /**
-     * Compare hosts with the referring url
-     */
-
-    var referringURI = parseURI(document.referrer);
-
-    // internal referrer
-    if (referringURI.host == window.location.host) { // if the hosts are the same... internal URL
-
-        info.external_url = false; // this will tell the script later that we are using internal URL
-        info.internal_url = document.referrer;
-
-    // external referrer
-    } else {
-
-        info.external_url = document.referrer;
-        info.internal_url = false; // same as above, let the script later know this is internal
-
-    }
-
-
-    /**
-     * We will assume the "medium" is other, but we will run other checks and
-     * override that if necessary
-     */
-
-    info.medium = 'other'; // default to 'other'
-    var utm_medium = getParameterByName('utm_medium'); // parsing our current URL for this query param
-
-
-    /**
-     * Check to see if it is a PPC referral
-     */
-
-    var query = window.location.search; // we just want the query string
-
-
-    if (info.external_url) {
-        info.medium = 'referring';
-    }
-
-    // seo
-    // if the above didn't fire (it takes priority), then lets see if its from a search engine via the referring URL
-    // I'm combining all of the search engines into this regex and then we will use the match to return the engine
-    var seo_matches = document.referrer.match(new RegExp('('+ search_engines.join('|') + ')', 'i'));
+    var seo_matches = document.referrer.match(new RegExp('('+ this.searchEngines.join('|') + ')', 'i'));
 
     if (seo_matches) {
-
-        info.medium = 'seo';
-        info.source = seo_matches[1].split('.')[0];
-
-        var queryMatches = document.referrer.match(/q\=([^\&]+)/); // grab the "search query" -- usually in a "q" get variable
-
-        if (info.source == 'yahoo') { // they just have to be difficult (uses 'p' instead of 'q' for search)
-            queryMatches = document.referrer.match(/p\=([^\&]+)/); // grab the "search query" -- usually in a "q" get variable
-        }        
-
-        if (queryMatches && queryMatches.length === 2) {
-            info.term = queryMatches[1].split('+').join(' '); // spaces are turned into pluses, so switch those out
-        } else {
-            info.term = false; // well, we didn't have a search.  the user could have been logged in.
-        }
-
+        return { type: 'seo', source: seo_matches[1].split('.')[0] };
     }
 
-    // Perhaps it is a PPC request
-    if (utm_medium == 'cpc') {
-        info.medium = 'ppc';
-        info.source = getParameterByName('utm_source');
-        info.term = getParameterByName('utm_term');
+    if (this.getParameterByName('utm_medium') == 'cpc') {
+        
+        return { type: 'ppc', source: this.getParameterByName('utm_source') };
     }
 
+    return false;
+
+};
+
+Leads.prototype.getSEOData = function() {
+
+
+    var queryMatches = document.referrer.match(/q\=([^\&]+)/); // grab the "search query" -- usually in a "q" get variable
+
+
+    if (info.source == 'yahoo') { // they just have to be difficult (uses 'p' instead of 'q' for search)
+        queryMatches = document.referrer.match(/p\=([^\&]+)/); // grab the "search query" -- usually in a "q" get variable
+    }
+
+    if (queryMatches && queryMatches.length === 2) {
+        return queryMatches[1].split('+').join(' '); // spaces are turned into pluses, so switch those out
+    }
+        
+    return false;
+};
+
+Leads.prototype.getPPCData = function() {
+    return this.getParameterByName('utm_term');
+};
+
+Leads.prototype.getExtraData = function() {
     // grab all of the other fields regardless of the medium definition (they may still be there)
-    info.campaign = getParameterByName('utm_campaign');
-    info.content = getParameterByName('utm_content');
-    
+    return {
+        campaign: this.getParameterByName('utm_campaign'),
+        content: this.getParameterByName('utm_content')
+    };
+};
 
+Leads.prototype.getParameterByName = function(name, queryString) {
 
+    // Thanks to Stackoverflow for the bulk of this code.  Looks like we are adjusting the potential arrays
+    // inside of our query string
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
 
-    info.internal_url = (info.internal_url || ''); // if our internal referrer is false, just make it blank
+    // Based on the property name grab the potential content immediately following it.
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec((queryString || window.location.search));// handle if they pass a URI directly or just use the current one
 
-    if (!getCookie(cookieName)) { // we don't have a cookie, so lets serialize our data and save it
-        writeCookie(cookieName, JSON.stringify(info), 30);
-    }
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); // decode what we have returned
 
-    var internal = info.internal_url;
-    var cookie = JSON.parse(getCookie(cookieName)); // turn our cookie into an object
+};
 
-    // if cookie is set, and user is coming in from another URL -- meaning 
-    // the user strolled back over here from another application but using the same session
-    if (cookie.external_url != info.external_url) {
+Leads.prototype.save = function() {
+    this.writeCookie(JSON.stringify(this.output), 30);
+    this.info = JSON.parse(this.getCookie());
+};
 
-    	// only do this if we know the original request was listed as "other"
-    	if (cookie.medium == 'other') { // if the current source is "other" then whatever.  rewrite anyway.
-	        // re-write external referrer
-	        writeCookie(cookieName, JSON.stringify(info), 30); // keep the cookie active for 30 days
-	    }
+Leads.prototype.updateForm = function() {
 
-    }
+    var $ = this.getElement;
 
-    info = cookie; // rename our cookie object to 'info'
+    // checking to see if we have at least one form on the page
+    if ($('form').length > 0) {
 
-    log(info); // if debugging is on, show a list of everything we are keeping track on each request
+        var gravity = $('.gfield').length > 0; // Tell us if we should handle gravity forms
 
-    // if a form exists on the page, go ahead and add our values to it (avoid empty values)
-    window.onload = function() {
+        for (var i = 0; i < this.inputNames.length; i++) {
 
-        // checking to see if we have at least one form on the page
-        if ($('form').length > 0) {
+            // now we are looking to find all of the fields that we defined way at the top
+            var field = this.inputNames[i],
+                elm = gravity ? $('.'+ this.prefix + field + ' input') : $('[name='+ this.prefix + field + ']');
 
-            var gravity = $('.gfield').length > 0; // Tell us if we should handle gravity forms
+            this.log(elm);
 
-            for (var i = 0; i < inputNames.length; i++) {
+            if (elm.length > 0) { // we have a match
 
-                // now we are looking to find all of the fields that we defined way at the top
-                var field = inputNames[i],
-                    elm = gravity ? $('.'+ prefix + field + ' input') : $('[name='+ prefix + field + ']');
-
-                log(elm);
-
-                if (elm.length > 0) { // we have a match
-
-                    // if the internal data isn't actually defined, just put a string... otherwise show our findings
-                    elm[0].value = (typeof info[field] !== 'undefined') ? info[field] : '';
-                }
-
+                // if the internal data isn't actually defined, just put a string... otherwise show our findings
+                elm[0].value = (typeof this.output[field] !== 'undefined') ? this.output[field] : '';
             }
+
+        }
+    }
+};
+
+Leads.prototype.get = function(name) {
+    return this.info[name];
+};
+
+(function() {
+
+    var leads = new Leads('__gfLeads', 'gf_', true);
+    if (!document.querySelectorAll) {
+        
+        if (console) {
+            console.log('leads.js cannot run without browser support for querySelectorAll');
         }
 
-    };
+        return false;
+    }
+    
+    leads.updateForm();
+
 
 })();
